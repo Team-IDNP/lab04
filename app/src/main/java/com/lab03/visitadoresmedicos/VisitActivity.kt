e2e9794a9140484135e2a9de0d992b73b7a25961
Araozu <faraoz@unsa.edu.pe>
2021-10-04 17:24:14 -0500

    Crear clase para activity Visita

---
+++ app/src/main/java/com/lab03/visitadoresmedicos/VisitActivity.kt
@@ -0,0 +1,18 @@
+package com.lab03.visitadoresmedicos
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class VisitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit)
+    }
+
+    fun regiterData(view: View) {
+
+    }
+
+}
