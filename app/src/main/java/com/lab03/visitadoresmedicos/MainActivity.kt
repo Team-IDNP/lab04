221e3573a7e088790492bcbec66a2127d835b958
Araozu <faraoz@unsa.edu.pe>
2021-10-04 18:13:35 -0500

    Implementar boton para ir a actividad Visita

--- app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.kt
+++ app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.lab03.visitadoresmedicos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun irAVisita(view: View) {
+        val i = Intent(this, VisitActivity::class.java)
+        startActivity(i)
+    }
+
 }
