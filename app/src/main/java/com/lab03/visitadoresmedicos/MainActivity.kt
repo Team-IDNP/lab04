2ebfbbd958ad7f283867f42387ed3d6fe1c297f2
gustavosand <gustavo123.ght69@gmail.com>
2021-10-04 20:49:36 -0500

    vista paciente

--- app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.kt
+++ app/src/main/java/com/lab03/visitadoresmedicos/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.lab03.visitadoresmedicos
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
     fun irAVisita(view: View) {
         val i = Intent(this, VisitActivity::class.java)
         startActivity(i)
+    }
+
+    var irAActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data
+            ////////
+        }
+    }
+
+    fun irARegistrarPaciente(view: View) {
+        val intent = Intent(this, PacienteActivity::class.java)
+        irAActivity.launch(intent)
     }
 
 }
---
+++ app/src/main/java/com/lab03/visitadoresmedicos/PacienteActivity.kt
@@ -0,0 +1,11 @@
+package com.lab03.visitadoresmedicos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PacienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paciente)
+    }
+}
